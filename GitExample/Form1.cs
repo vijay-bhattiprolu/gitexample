358703c97dbdb2cc603959ad59e8a4507af0ef2e
vijaya bhaskar <vijaya.bhattiprolu@evry.com>
2020-04-20 13:00:32 +0530

    show mess

--- GitExample/Form1.cs
+++ GitExample/Form1.cs
@@ -16,5 +16,10 @@ namespace GitExample
         {
             InitializeComponent();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("hi");
+        }
     }
 }
